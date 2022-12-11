30afc3b69c6c3b9999b1321a53051725b53072d8
drdv <mail.mitko@gmail.com>
2022-12-11 07:31:54 +0000

    Deploying to gh-pages from @ drdv/triangle@53a218fe5c93539eb3e0c94c43a93c0ff6ffb694 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev3+gf3495e8',
+    VERSION: '0.1.dev4+g53a218f',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
